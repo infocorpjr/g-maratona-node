6da71df82546c4a2164f02919ba4ab4722b89474
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-07-24 18:10:08 -0400

    Fist commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express, { json } from 'express';
+const app = express();
+import { config } from 'dotenv';
+import morgan from 'morgan';
+
+// Load .env
+config();
+
+// eslint-disable-next-line no-unused-vars
+import colors from 'colors'
+
+const PORT = process.env.APP_PORT || 5000;
+
+app.use(json());
+
+if(process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'));
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Microservice is running at port ${PORT}`.cyan.bold)
+});
